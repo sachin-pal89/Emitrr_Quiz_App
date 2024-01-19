9667cc5faadc515b8419a37abd206295639b7f0d
Sachin Pal <sachinpal01012003@gmail.com>
2024-01-19 19:38:07 +0530

    New Changes Made

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Authentication from "./pages/Authentication/Authentication";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" exact element={<Authentication />} />
-        <Route path="/home" exact element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- client/src/pages/Authentication/Authentication.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Authentication = () => {
-    return (
-        <div>authentication</div>
-    )
-}
-
-export default Authentication
